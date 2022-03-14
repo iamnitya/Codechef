10bb8162d3276ae49cc3b97756c95ceca1798771
Nitya Anuga <93179435+iamnitya@users.noreply.github.com>
2022-03-14 19:58:41 +0530

    Create Which-Division.py

---
+++ Which Division/Which-Division.py
@@ -0,0 +1,10 @@
+test_cases=int(input())
+for i in range(test_cases):
+    a=int(input())
+    if a<1600:
+        print(3)
+    elif 1600<=a<2000:
+        print(2)
+    else:
+        print(1)
+    
