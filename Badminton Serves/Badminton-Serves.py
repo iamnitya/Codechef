f6ce9d61fff2ebf926356c679907cb6b29261770
Nitya Anuga <93179435+iamnitya@users.noreply.github.com>
2022-03-14 23:32:08 +0530

    Create Badminton-Serves.py

---
+++ Badminton Serves/Badminton-Serves.py
@@ -0,0 +1,9 @@
+test_cases=int(input())
+for i in range(test_cases):
+    count=0
+    n=int(input())
+    for i in range(n+1):
+        if i%2==0:
+            count+=1 
+    print(count)
+        
