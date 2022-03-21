b65f4aebf5c3689074ac6d5c8a92667668d72d84
Nitya Anuga <93179435+iamnitya@users.noreply.github.com>
2022-03-21 08:40:42 +0530

    Create Tanu-and-Head-bob.py

---
+++ Tanu and Head-bob/Tanu-and-Head-bob.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    n=int(input())
+    s=input()
+    if "I" in s:
+        print("INDIAN")
+    elif "Y" in s:
+        print("NOT INDIAN")
+    else:
+        print("NOT SURE")# cook your dish here
