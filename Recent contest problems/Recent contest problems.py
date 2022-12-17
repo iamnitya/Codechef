aed9937523423c5055c1e4b14c3a2549221acb40
Nitya Anuga <93179435+iamnitya@users.noreply.github.com>
2022-12-17 17:31:26 +0530

    Create Recent contest problems.py

---
+++ Recent contest problems/Recent contest problems.py
@@ -0,0 +1,7 @@
+test_cases = int(input())
+for i in range(test_cases):
+    c = int(input())
+    a = input().split()
+    start_count = a.count("START38")
+    lifetime_count = a.count("LTIME108")
+    print(start_count,lifetime_count)                                                                                                                                                                                                                           
